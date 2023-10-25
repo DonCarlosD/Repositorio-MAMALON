5525a6c71c9a411f2117f92be9e3ed5d0bef7349
Joel Johs <jojohersa21@gmail.com>
2023-10-25 13:02:07 -0600

    Me rendi con la imagen

--- front/src/app/JojoPage/page.js
+++ front/src/app/JojoPage/page.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Toji from './img/Toji.jpg';
 
 function Page() {
     const centerStyle = {
@@ -6,18 +7,25 @@ function Page() {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '100vh', // Esto centra verticalmente en la pantalla
-        backgroundColor: '#d3dce4', // Establece el color de fondo
-        color: '#202831', // Establece el color del texto
-      };
+        height: '100vh',
+        backgroundColor: '#d3dce4',
+        color: '#202831',
+    };
 
-      return (
+    return (
         <div style={centerStyle}>
-          <h1>Jojo Presenta:</h1>
-          <p>Fushiguro Toji, el mas vergas</p>
-          <<img src="/img/Toji.jpg" alt="Mi Imagen" />
+            <h1>Jojo Presenta:</h1>
+            <p>Fushiguro Toji, el más increíble</p>
+            <img src={Toji} alt="Fushiguro Toji" />
+            <p> Toji Fushiguro (伏黒甚爾 Fushiguro Tōji?) previamente conocido como Toji Zenin (禪院甚爾 Zen'in Tōji?), 
+                es uno de los personajes de la serie manga Jujutsu Kaisen. Fue miembro del Clan Zenin y el padre de Megumi Fushiguro 
+                y Tsumiki Fushiguro.
+                Durante sus días trabajando como Usuario Maldito luego de abandonar el Clan Zenin, 
+                recibió un trabajo de la Asociación de Vasijas del Tiempo con el fin de asesinar a la próxima 
+                Vasija de la Sustancia Estelar del Señor Tengen. Falleció como resultado de su enfrentamiento 
+                contra Satoru Gojo y antes de morir, dejó a Megumi en su cuidado.</p>
         </div>
-      );
+    );
 }
 
 export default Page;
