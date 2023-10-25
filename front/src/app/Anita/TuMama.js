4776f786b2addff92e00c9d33286eadda38c88d9
Aneta66 <l21120169@morelia.tecnm.mx>
2023-10-25 12:43:28 -0600

    boton

--- front/src/app/Anita/TuMama.js
+++ front/src/app/Anita/TuMama.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const page=() =>{
-    return(
-        <dv>
-        <h1>Hola mundo</h1>
-        </dv>
-    )
-   
+const Page = () => {
+    const headerStyle = {
+        borderBottom: '2px solid blue',
+        paddingBottom: '10px',
+        marginBottom: '20px'
+    };
+
+    return (
+        <div>
+            <div style={headerStyle}>
+                <h1>Mi pagina mamalona</h1>
+            </div>
+            <button>Touch me</button>
+        </div>
+    );
 }
 
-export default page
+export default Page;
