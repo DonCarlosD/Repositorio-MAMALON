8a33396c6b2e5943af464577b5bec3b2701686f8
Aneta66 <l21120169@morelia.tecnm.mx>
2023-10-25 13:08:06 -0600

    Color_Fondo

--- front/src/app/Anita/page.js
+++ front/src/app/Anita/page.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const page = () => {
+    const pageStyle = {
+        backgroundColor: 'lightblue', // Fondo azul cielo
+    };
+
     const headerStyle = {
         borderBottom: '2px solid blue',
         paddingBottom: '10px',
-        marginBottom: '20px'
+        marginBottom: '20px',
+        backgroundColor: 'yellow', // Relleno del encabezado en color amarillo
     };
 
     return (
-        <div>
+        <div style={pageStyle}>
             <div style={headerStyle}>
                 <h1>Mi pagina mamalona</h1>
             </div>
@@ -16,4 +21,5 @@ const page = () => {
         </div>
     );
 }
-export default page
+
+export default page;
