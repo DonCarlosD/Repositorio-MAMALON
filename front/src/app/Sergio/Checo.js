4cc00c75b1172798c83ef4a3cc34f35c04c6cef2
Sergio Jarek magana Vazquez <l20121008@morelia.tecnm.mx>
2023-10-25 12:59:08 -0600

    cambiosSergio2

--- front/src/app/Sergio/Checo.js
+++ front/src/app/Sergio/Checo.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react'
 
-const page = () => {
-    return (
-        <h1> Checo Perez es la onda </h1>
-    )
+
+// Función para calcular el área de un círculo dado su radio
+function calcularAreaCirculo(radio) {
+    return Math.PI * radio * radio;
 }
+
+// Ejemplo de uso
+const radio = 5;
+const area = calcularAreaCirculo(radio);
+console.log(`El área del círculo con radio ${radio} es ${area.toFixed(2)}`);
+
+export default page
