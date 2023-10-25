0b71ef38b35409619966a614aca555c108b66ccb
DonCarlosD <l20121019@morelia.tecnm.mx>
2023-10-25 13:38:49 -0600

    lore de one pice

--- front/src/app/Charly/page.js
+++ front/src/app/Charly/page.js
@@ -3,9 +3,34 @@ import React from 'react'
 const page=() =>{
     return(   
         <div>
-            <h1>Animes Populares</h1>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4JdRu02Q0MyM2mdwFIl97wsxq320tswa7xsJWEctkV3f8EJQDcwisK5AznOWO88YeL8&usqp=CAU" alt="Imagen de anime popular" />
-        </div>
+        <header>
+        <h1>Lore de One Piece</h1>
+    </header>
+    <main>
+        <section id="introduccion">
+            <h2>Introducción</h2>
+            <p>
+                "One Piece" es un manga y anime creado por Eiichiro Oda. La historia sigue a Monkey D. Luffy,
+                un joven pirata con la ambición de encontrar el One Piece y convertirse en el Rey de los Piratas.
+            </p>
+        </section>
+        <section id="mundo">
+            <h2>El Mundo de "One Piece"</h2>
+            <p>
+                El mundo de "One Piece" está lleno de islas, mares, piratas, marineros y criaturas únicas.
+                La Gran Ruta de Grand Line y la isla de Raftel son elementos clave en la historia.
+            </p>
+        </section>
+        <section id="personajes">
+            <h2>Personajes Principales</h2>
+            <p>
+                Entre los personajes principales se encuentran Monkey D. Luffy, Roronoa Zoro, Nami, Usopp, Sanji,
+                Nico Robin, Franky, Brook, Tony Tony Chopper y muchos otros.
+                
+            </p>
+        </section>
+    </main>
+    </div>
     );
 }
 
