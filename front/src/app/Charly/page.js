d051b662953af256521d7c2edd92d09935adc955
DonCarlosD <l20121019@morelia.tecnm.mx>
2023-10-25 13:21:58 -0600

    agregar imagen

---
+++ front/src/app/Charly/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page=() =>{
+    return(   
+        <div>
+            <h1>Animes Populares</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM4JdRu02Q0MyM2mdwFIl97wsxq320tswa7xsJWEctkV3f8EJQDcwisK5AznOWO88YeL8&usqp=CAU" alt="Imagen de anime popular" />
+        </div>
+    );
+}
+
+export default page
